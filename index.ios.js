4da45f35c2df52c1b4ae18e1848bf5917c758118
adtm <tomas.eglinskas@gmail.com>
2017-09-17 02:16:52 +0300

    ios: Changed to calendar list and added visible markers

--- index.ios.js
+++ index.ios.js
@@ -18,30 +18,20 @@ export default class tombnb extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Calendar
-          // Initially visible month. Default = Date()
-          current={'2012-03-01'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2012-05-10'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2012-05-30'}
-          // Handler which gets executed on day press. Default = undefined
-          onDayPress={(day) => { console.log('selected day', day) }}
-          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'yyyy MM'}
-          // Handler which gets executed when visible month changes in calendar. Default = undefined
-          onMonthChange={(month) => { console.log('month changed', month) }}
-          // Hide month navigation arrows. Default = false
-          hideArrows={true}
-          // Do not show days of other months in month page. Default = false
-          hideExtraDays={true}
-          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
-          // day from another month that is visible in calendar page. Default = false
-          disableMonthChange={true}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-          firstDay={1}
-          // Hide day names. Default = false
-          hideDayNames={true}
+        <CalendarList
+          // Callback which gets executed when visible months change in scroll view. Default = undefined
+          onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+          // Max amount of months allowed to scroll to the past. Default = 50
+          pastScrollRange={50}
+          // Max amount of months allowed to scroll to the future. Default = 50
+          futureScrollRange={50}
+          // Enable or disable scrolling of calendar list
+          scrollEnabled={true}
+          markedDates={{
+            '2017-09-09': { selected: true },
+            '2017-09-19': { marked: true },
+            '2017-09-29': { disabled: true }
+          }}
         />
       </View>
     );
