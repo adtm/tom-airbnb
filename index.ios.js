44510c29bdbd154a160ad8ac302a6d15087bbb99
adtm <tomas.eglinskas@gmail.com>
2017-09-17 02:03:55 +0300

    ios: Added initial calendar component

--- index.ios.js
+++ index.ios.js
@@ -12,20 +12,37 @@ import {
   View
 } from 'react-native';
 
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
 export default class tombnb extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Calendar
+          // Initially visible month. Default = Date()
+          current={'2012-03-01'}
+          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+          minDate={'2012-05-10'}
+          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+          maxDate={'2012-05-30'}
+          // Handler which gets executed on day press. Default = undefined
+          onDayPress={(day) => { console.log('selected day', day) }}
+          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+          monthFormat={'yyyy MM'}
+          // Handler which gets executed when visible month changes in calendar. Default = undefined
+          onMonthChange={(month) => { console.log('month changed', month) }}
+          // Hide month navigation arrows. Default = false
+          hideArrows={true}
+          // Do not show days of other months in month page. Default = false
+          hideExtraDays={true}
+          // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+          // day from another month that is visible in calendar page. Default = false
+          disableMonthChange={true}
+          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+          firstDay={1}
+          // Hide day names. Default = false
+          hideDayNames={true}
+        />
       </View>
     );
   }
@@ -33,21 +50,9 @@ export default class tombnb extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    marginTop: 40,
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('tombnb', () => tombnb);
