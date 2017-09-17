c08a9ab3f63cd624f84b5e82dbeb5ef563fcbaac
adtm <tomas.eglinskas@gmail.com>
2017-09-17 13:15:56 +0300

    Added book button

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import List from 'antd-mobile/lib/list';
 import TextareaItem from 'antd-mobile/lib/textarea-item';
+import Button from 'antd-mobile/lib/button';
  
 import { createForm } from 'rc-form';
 
@@ -42,20 +43,13 @@ class ModalScreen extends Component {
   render() {
     const { getFieldProps } = this.props.form;
     return (
-      <ScrollView>
-        <List renderHeader={() => 'Customize to focus'}>
+      <View >
+        <List renderHeader={() => 'Person information'}>
           <TextareaItem
-            title="标题"
-            placeholder="auto focus in Alipay client"
-            data-seed="logId"
-            autoFocus
-            autoHeight
-          />
-          <TextareaItem
-            title="标题"
-            placeholder="click the button below to focus"
-            data-seed="logId"
-            autoHeight
+            {...getFieldProps('control')}
+            placeholder="Name"
+            data-seed="name"
+            clear
             focused={this.state.focused}
             onFocus={() => {
               this.setState({
@@ -63,77 +57,14 @@ class ModalScreen extends Component {
               });
             }}
           />
-        </List>
-        <List renderHeader={() => 'Whether is controlled'}>
           <TextareaItem
-            {...getFieldProps('control')}
-            title="受控组件"
-            placeholder="Hello World"
-          />
-          <TextareaItem
-            title="非受控组件"
-            placeholder="please input content"
-          />
-        </List>
-        <List renderHeader={() => 'Auto / Fixed height'}>
-          <TextareaItem
-            {...getFieldProps('note3')}
-            title="高度自适应"
-            autoHeight
-            labelNumber={5}
-          />
-          <TextareaItem
-            {...getFieldProps('note1')}
-            rows={3}
-            placeholder="fixed number of lines"
-          />
-        </List>
-        <List renderHeader={() => 'Show clear icon'}>
-          <TextareaItem
-            {...getFieldProps('clear1')}
+            placeholder="Surname"
+            data-seed="surname"
             clear
-            title="标题"
-            placeholder="displayed clear icon while typing"
           />
         </List>
-        <List renderHeader={() => 'Custom title（text / image / empty）'}>
-          <TextareaItem
-            {...getFieldProps('title3')}
-            title={<img src="https://os.alipayobjects.com/rmsportal/mOoPurdIfmcuqtr.png" alt="icon" style={{ width: '0.56rem', height: '0.56rem' }} />}
-            placeholder="title can be customized"
-          />
-        </List>
-        <List renderHeader={() => 'Limited value length'}>
-          <TextareaItem
-            {...getFieldProps('note4')}
-            placeholder="can enter up to 10 characters"
-            count={10}
-          />
-        </List>
-        <List renderHeader={() => 'Count'}>
-          <TextareaItem
-            {...getFieldProps('count', {
-              initialValue: '计数功能,我的意见是...',
-            })}
-            rows={5}
-            count={100}
-          />
-        </List>
-        <List renderHeader={() => 'Not editable / Disabled'}>
-          <TextareaItem
-            {...getFieldProps('note6', {
-              initialValue: 'not editable',
-            })}
-            title="姓名"
-            editable={false}
-          />
-          <TextareaItem
-            value="disabled style"
-            title="姓名"
-            disabled
-          />
-        </List>
-      </ScrollView>
+        <Button style={{ margin: 10}} type="primary" >Book</Button>
+      </View>
     );
   }
 }
