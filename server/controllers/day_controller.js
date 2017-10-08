2b903d9efd1e77c13347d37683b2d3a526d65a7a
adtm <tomas.eglinskas@gmail.com>
2017-10-09 00:43:03 +0300

    Booking / Day reset refactor

--- server/controllers/day_controller.js
+++ server/controllers/day_controller.js
@@ -1,25 +1,43 @@
 const async = require('async');
+const Booking = require('../models/booking');
 const Day = require('../models/day');
 const moment = require('moment');
 
 function resetDays(req, res) {
+
+  Day
+    .remove()
+    .then(() => console.log('Day data deleted!'))
+    .catch(err => console.log(err));
+
+  Booking
+    .remove()
+    .then(() => console.log('Booking data deleted!'))
+    .catch(err => console.log(err));
+
   var createUser = function(respose, callback) {
     callback(null, {
         date: response
     });
   };
+
   async.times(50, function(n, next) {
-    var m = moment.utc();
-    m.set({hour:0,minute:0,second:0,millisecond:0});
-    m.add(n, 'days');
-    m.toISOString();
-    const date = m.format();
-    const day = new Day({ date, bookings: [] });
-      day.save(n, function(err, user) {
-          next(err, user);
-      });
-  }, function(err, dates) {
-      res.json(dates)
+    var m = moment
+      .utc()
+      .set({hour:0, minute:0, second:0, millisecond:0})
+      .add(n, 'days')
+      .toISOString();
+
+    const date = moment(m).format();
+    Day
+      .create({date, bookings: []})
+      .then(response => next(null, response))
+      .catch(err => next(err, null)) 
+
+  }, (err, response) => {
+    if (err)
+      res.status(404).send({ err });
+    res.status(200).send(response);
   });
 }
 
