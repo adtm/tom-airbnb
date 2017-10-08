5644174c1a683f827506f0a519a41b50224ce892
adtm <tomas.eglinskas@gmail.com>
2017-10-08 12:33:37 +0300

    Added month limit

--- server/controllers/booking_controller.js
+++ server/controllers/booking_controller.js
@@ -26,36 +26,60 @@ function createBooking(req, res, next) {
     moment(bookerTime, 'HH:mm').format('HH:mm'),
     'YYYY-MM-DD HH:mm').format('YYYY-MM-DD HH:mm');
 
-  Day
-    .findOne({ date: date })
-    .then(foundDay => {
-      if (!foundDay) {
-        Booking
-          .create({ bookerName, bookerSurname, bookerTime: datef })
-          .then(
-          response => {
-            Day
-              .create({ bookings: bookings.concat(response) })
-              .then(response => res.status(200).send(response))
-              .catch(err => errorResponse('day', err));
-          }).catch(err => errorResponse('booking', err));
-      } 
+  // limit--checking
+  const year_limit = moment(date).year();
+  const month_limit = moment(date).month();
 
-      Booking
-        .create({ bookerName, bookerSurname, bookerTime: datef })
-        .then(
-        response => {
-          // @TODO change later to a regular map
-          foundDay.bookings.push(response);
-          foundDay.bookings.sort((a, b) => {
-            return moment(a.bookerTime).valueOf() - moment(b.bookerTime).valueOf();
+  // @TODO fix upper date
+  const lower_date = moment(new Date(year_limit, month_limit, 1)).format();
+  const upper_date = moment(new Date(year_limit, month_limit + 1, 0)).format();
+
+  Day
+    .find({ date: { $gte: lower_date, $lte: upper_date } })
+    .then(response => {
+      const bookings = [];
+      response.map(date => {
+        date.bookings.map(booking => {
+          bookings.push(booking);
+        })
+      });
+
+      if (bookings.length > 8) {
+        res.status(400).send(errorResponse('limit', 'Exceeded month limit'));
+      }
+      else {
+        Day
+          .findOne({ date: date })
+          .then(foundDay => {
+            if (!foundDay) {
+              Booking
+                .create({ bookerName, bookerSurname, bookerTime: datef })
+                .then(
+                response => {
+                  Day
+                    .create({ bookings: bookings.concat(response) })
+                    .then(response => res.status(200).send(response))
+                    .catch(err => errorResponse('day', err));
+                }).catch(err => errorResponse('booking', err));
+            }
+
+            Booking
+              .create({ bookerName, bookerSurname, bookerTime: datef })
+              .then(
+              response => {
+                // @TODO change later to a regular map
+                foundDay.bookings.push(response);
+                foundDay.bookings.sort((a, b) => {
+                  return moment(a.bookerTime).valueOf() - moment(b.bookerTime).valueOf();
+                })
+                foundDay
+                  .save()
+                  .then(response => res.status(200).send(response))
+                  .catch(err => errorResponse('booking', err));
+              }).catch(err => errorResponse('booking', err));
           })
-          foundDay
-            .save()
-            .then(response => res.status(200).send(response))
-            .catch(err => errorResponse('booking', err));
-        }).catch(err => errorResponse('booking', err));
-    })
+      }
+    });
 }
 
 module.exports = {
